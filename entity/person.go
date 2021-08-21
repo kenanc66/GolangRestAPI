167d67a9e58e138ef5d14999abd9da9265b61801
kenankenanc66 <kenancicekdev@gmail.com>
2021-08-21 14:57:21 +0300

    RESTAPI DONE

---
+++ entity/person.go
@@ -0,0 +1,10 @@
+package entity
+
+//Person object for REST(CRUD)
+type Person struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+	Phone     string `json:"phone"`
+}
